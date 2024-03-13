97667c4fe3a1d87d05da97f14de71522414e571c
은성 <jyjmjs2@naver.com>
2024-03-13 20:31:10 +0900

    24.03.13

---
+++ 구현/BOJ_7287.java
@@ -0,0 +1,9 @@
+package Implementation;
+
+// 24.03.13
+public class BOJ_7287 {
+    public static void main(String[] args) {
+        System.out.println("112");
+        System.out.println("jyjmjs2");
+    }
+}
