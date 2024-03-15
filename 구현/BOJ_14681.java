dbd58f3f58339a2b89b5018fb8f04870de0ebe06
은성 <jyjmjs2@naver.com>
2024-03-15 21:41:32 +0900

    24.03.15

---
+++ 구현/BOJ_14681.java
@@ -0,0 +1,24 @@
+package Implementation;
+
+import java.io.*;
+
+// 24.03.15
+public class BOJ_14681 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if(x > 0) {
+            if(y > 0) bw.write("1");
+            else bw.write("4");
+        } else {
+            if(y > 0) bw.write("2");
+            else bw.write("3");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
