151ab2c61510cf770cf1abc81b0460a30875f1e9
은성 <jyjmjs2@naver.com>
2024-03-25 21:44:09 +0900

    24.03.25

---
+++ 수학/BOJ_2738.java
@@ -0,0 +1,35 @@
+package Math;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// 24.03.25
+public class BOJ_2738 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
