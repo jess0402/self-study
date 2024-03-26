f198006e3601243dff8f9ce8d34e5576f1f31cdf
은성 <jyjmjs2@naver.com>
2024-03-26 21:10:50 +0900

    24.03.26

---
+++ 구현/BOJ_11718.java
@@ -0,0 +1,16 @@
+package Implementation;
+
+import java.io.*;
+
+// 24.03.26
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while((str=br.readLine())!=null) {
+            System.out.println(str);
+        }
+        br.close();
+    }
+}
