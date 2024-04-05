1b6c7f5b4d37be148565b44d7c5a79f0293606cd
은성 <jyjmjs2@naver.com>
2024-04-05 23:13:57 +0900

    24.04.05

---
+++ 구현/BOJ_2480.java
@@ -0,0 +1,32 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 24.04.05
+public class BOJ_2480 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(a == b && b == c) {
+            System.out.println(10000 + (a * 1000));
+        } else if(a != b && b != c && c != a) {
+            System.out.println(Math.max(a, Math.max(b, c)) * 100);
+        } else {
+            if(a == b) {
+                System.out.println(1000 + (a * 100));
+            } else if(b == c){
+                System.out.println(1000 + (b * 100));
+            } else if(c == a) {
+                System.out.println(1000 + (c * 100));
+            }
+        }
+    }
+}
