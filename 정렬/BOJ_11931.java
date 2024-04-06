aa89434b4bf8936261052eb40db4e34a7a638301
은성 <jyjmjs2@naver.com>
2024-04-06 22:37:42 +0900

    24.04.06

---
+++ 정렬/BOJ_11931.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+// 24.04.06
+public class BOJ_11931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int i = 0; i < N; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
