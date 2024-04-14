a5f6e39de766e2cf5179e98802a972f76eeacaae
은성 <jyjmjs2@naver.com>
2024-04-14 21:55:07 +0900

    24.04.14

---
+++ 정렬/BOJ_15688.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Arrays;
+
+public class BOJ_15688 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
