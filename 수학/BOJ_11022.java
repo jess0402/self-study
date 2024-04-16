64dbeee9c6b97cbaf590410d3af5d87c46fd2a69
은성 <jyjmjs2@naver.com>
2024-04-17 00:00:00 +0900

    24.04.16

---
+++ 수학/BOJ_11022.java
@@ -0,0 +1,24 @@
+package Math;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + i +": " + A + " + " + B + " = " + (A+B) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
